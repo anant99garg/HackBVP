69bc930fec854fa933cf1eb676c1a5a4fbf8a45e
Mohammad Ahmad <ahmadofficial242@gmail.com>
2018-09-29 04:26:52 +0530

    clean code

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-var express         = require('express'),
-    app             = express(),
-    bodyParser      = require('body-parser'),
-    methodOverride  = require("method-override"),
-    mongoose        = require('mongoose'),
-    request         = require('request'),
-    passport        = require('passport'),
-    LocalStrategy   = require('passport-local'),
-    path            = require('path'),
-    multer          = require('multer');
-var morgan          = require('morgan');
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    methodOverride = require("method-override"),
+    mongoose = require('mongoose'),
+    request = require('request'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local'),
+    path = require('path'),
+    multer = require('multer');
+var morgan = require('morgan');
 
 var organisationData = require("./models/org");
 var NewUser = require("./models/user");
@@ -16,9 +16,10 @@ var NewUser = require("./models/user");
 
 mongoose.connect('mongodb://localhost/DAVA');
 app.use(morgan('combined'));
-//app.use('/show_profile',express.static(__dirname +'/uploads'));
-//app.use('/profiles',express.static(__dirname +'/uploads'));
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
@@ -47,7 +48,7 @@ app.use(function (req, res, next) {
 });
 
 
-app.get("/",function(req, res){
+app.get("/", function (req, res) {
     res.render("index");
 })
 
@@ -59,24 +60,20 @@ app.get("/",function(req, res){
 /////////FOR USER SIGN UP///////////////
 ///////////////////////////////////////
 
-app.get("/login",function(req, res){
+app.get("/login", function (req, res) {
     res.render("login");
 })
-app.post("/login", passport.authenticate("local",
-    {
-        successRedirect: "/org",
-        failureRedirect: "/login"
-    }), function (req, res) {
-    });
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/org",
+    failureRedirect: "/login"
+}), function (req, res) {});
 
 
 
-app.get("/signup",function(req, res){
+app.get("/signup", function (req, res) {
     res.render("signup")
 })
-// app.get("/dash",function(req, res){
-//     res.render("dashboard")
-// })
+
 app.post("/signup", function (req, res) {
     var username = req.body.username;
     var name = req.body.name;
@@ -87,17 +84,27 @@ app.post("/signup", function (req, res) {
     var latitude = req.body.latitude;
     var phonenumber = req.body.phonenumber;
     var affected = req.body.affected;
-     
-    var new_user ={username:username,latitude:latitude,longitude:longitude,name:name, phonenumber:phonenumber,affected:affected,organisation:organisation,email:email,address:address};
-NewUser.register(new_user,req.body.password, function(err, newUser){
-    if (err){
-        console.log(err);
-        res.redirect("Something went wrong");
-    } 
-    passport.authenticate("local")(req, res, function () {
-        res.redirect("/");
+
+    var new_user = {
+        username: username,
+        latitude: latitude,
+        longitude: longitude,
+        name: name,
+        phonenumber: phonenumber,
+        affected: affected,
+        organisation: organisation,
+        email: email,
+        address: address
+    };
+    NewUser.register(new_user, req.body.password, function (err, newUser) {
+        if (err) {
+            console.log(err);
+            res.redirect("Something went wrong");
+        }
+        passport.authenticate("local")(req, res, function () {
+            res.redirect("/");
+        });
     });
-});
 });
 
 
@@ -107,33 +114,41 @@ NewUser.register(new_user,req.body.password, function(err, newUser){
 ///////////////////////////////////////
 
 
-app.get("/org",isLoggedIn,function(req, res){
-    organisationData.find({},function(err, organisationD1){
-        if (err){
+app.get("/org", isLoggedIn, function (req, res) {
+    organisationData.find({}, function (err, organisationD1) {
+        if (err) {
             res.redirect('/login');
-        }else{
+        } else {
             console.log(organisationD1);
-            res.render('org',{organisationD2:organisationD1});
+            res.render('org', {
+                organisationD2: organisationD1
+            });
         };
     });
 });
-app.post("/org", isLoggedIn,function (req, res) {
+app.post("/org", isLoggedIn, function (req, res) {
     var namep = req.body.namep;
     var condition = req.body.condition;
     var guardianname = req.body.guardianname;
-    var phonenumberg = req.body.phonenumberg; 
-    var critical = req.body.critical; 
-    var newuser ={namep:namep, condition:condition,guardianname:guardianname,phonenumberg:phonenumberg,critical:critical};
-organisationData.create(newuser, function(err, organisationD){
-    if (err){
-        console.log(err);
-        res.redirect("Something went wrong");
-    } 
-    res.redirect('/org');
-});
+    var phonenumberg = req.body.phonenumberg;
+    var critical = req.body.critical;
+    var newuser = {
+        namep: namep,
+        condition: condition,
+        guardianname: guardianname,
+        phonenumberg: phonenumberg,
+        critical: critical
+    };
+    organisationData.create(newuser, function (err, organisationD) {
+        if (err) {
+            console.log(err);
+            res.redirect("Something went wrong");
+        }
+        res.redirect('/org');
+    });
 });
 
-app.get("/logout", function(req, res){
+app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/login");
 });
@@ -144,7 +159,7 @@ function isLoggedIn(req, res, next) {
     }
     res.redirect("/login");
 };
-    
+
 app.listen(process.env.PORT || 7000, function () {
-        console.log("your server is on from port 7000");
-    });
+    console.log("your server is on from port 7000");
+});
